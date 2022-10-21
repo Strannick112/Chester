ea8bda0f3813abfec269eafecb74d4e56d472fa0
stasik <stas960@gmail.com>
2022-10-21 21:09:39 +0300

    fixed stop_wipe crash

--- chester_bot/bot_commands/__init__.py
+++ chester_bot/bot_commands/__init__.py
@@ -6,3 +6,4 @@ from .give_items import give_items
 from .start_wipe import start_wipe
 from .stop_wipe import stop_wipe
 from .delete_claim import delete_claim
+from .checkout_marks_on_executed_claims import checkout_marks_on_executed_claims
