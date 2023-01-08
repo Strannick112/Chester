68ff10eeed7eb3dce06a12b1c309b9242d2cb17c
stasik <stas960@gmail.com>
2023-01-09 00:39:01 +0300

    Фикс работы символа $ в игровом чате

--- chesterbot/cogs/ServerManage.py
+++ chesterbot/cogs/ServerManage.py
@@ -129,7 +129,7 @@ class ServerManage(commands.Cog, name="Управление сервером"):
                 await self.log_channel.send(content=text)
                 if "[Say]" in text:
                     if re.findall(r': (.)', text)[0] != "$":
-                        await self.chat_channel.send(content=re.split(r'\)', text)[1])
+                        await self.chat_channel.send(content=re.findall(r'\) ([\w\W]+?)', text)[0])
                 if "[Announcement]" in text\
                         or "[Join Announcement]" in text\
                         or "[Leave Announcement]" in text:
