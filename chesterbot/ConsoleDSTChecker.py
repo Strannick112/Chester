0e27d23aa3d1e337f8f6489ef637e2c28bf6af67
stasik <stas960@gmail.com>
2023-05-28 12:57:03 +0300

    Рефакторинг

--- chesterbot/ConsoleDSTChecker.py
+++ chesterbot/ConsoleDSTChecker.py
@@ -45,7 +45,7 @@ class ConsoleDSTChecker:
                 text = file_iter.stdout.readline()[12:]
                 for reg_answer, command in self.__commands.items():
                     if command["shard_id"] == shard_id:
-                        if re.findall(reg_answer, text) is not []:
+                        if re.findall(reg_answer, text):
                             # print("The result if finding: ", re.findall(reg_answer, text))
                             print("The reg_answer is: ", reg_answer)
                             command["future"].set_result(text)
