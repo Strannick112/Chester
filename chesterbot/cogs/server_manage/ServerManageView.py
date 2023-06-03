9a7d4ce02472715c67f87a6c7ea5d1c9ddb3febb
stasik <stas960@gmail.com>
2023-06-04 01:10:16 +0300

    Добавление кнопок для управления игровым сервером в дискорд

--- chesterbot/cogs/server_manage/ServerManageView.py
+++ chesterbot/cogs/server_manage/ServerManageView.py
@@ -9,28 +9,28 @@ from chesterbot.cogs.server_manage.commands import restart, stop, soft_stop, sof
 class ServerManageView(View):
     @discord.ui.button(label="Запуск сервера", style=ButtonStyle.green)
     async def start_callback(self, interaction, button):
+        await restart()
         await interaction.response.send_message("Запуск сервера принят к исполнению")
-        return await restart()
 
     @discord.ui.button(label="Остановка сервера", style=ButtonStyle.red)
     async def stop_callback(self, interaction, button):
+        await stop()
         await interaction.response.send_message("Остановка сервера принята к исполнению")
-        return await stop()
 
     @discord.ui.button(label="Остановка сервера через 1 минуту", style=ButtonStyle.red)
     async def soft_stop_callback(self, interaction, button):
+        await soft_stop()
         await interaction.response.send_message("Остановка сервера через 1 минуту принята к исполнению")
-        return await soft_stop()
 
     @discord.ui.button(label="Перезапуск сервера", style=ButtonStyle.green)
     async def restart_callback(self, interaction, button):
+        await restart()
         await interaction.response.send_message("Перезагрузка сервера принята к исполнению")
-        return await restart()
 
     @discord.ui.button(label="Перезапуск сервера через 1 минуту", style=ButtonStyle.green)
     async def soft_restart_callback(self, interaction, button):
+        await soft_restart()
         await interaction.response.send_message("Перезагрузка сервера через 1 минуту принята к исполнению")
-        return await soft_restart()
 
     async def interaction_check(self, interaction: Interaction, /) -> bool:
         for role in interaction.user.roles:
