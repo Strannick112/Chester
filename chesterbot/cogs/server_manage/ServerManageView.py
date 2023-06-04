9c565752c09105f6cd78674d076bab8b296c169a
stasik <stas960@gmail.com>
2023-06-04 13:53:23 +0300

    Добавление кнопок для управления игровым сервером в дискорд

--- chesterbot/cogs/server_manage/ServerManageView.py
+++ chesterbot/cogs/server_manage/ServerManageView.py
@@ -7,32 +7,32 @@ from chesterbot.cogs.server_manage.commands import restart, stop, soft_stop, sof
 
 
 class ServerManageView(View):
-    @discord.ui.button(label="Запуск сервера", style=ButtonStyle.green)
+    @discord.ui.button(label="Запуск сервера", style=ButtonStyle.green, row=0)
     async def start_callback(self, interaction, button):
         await restart()
         await interaction.response.send_message("Запуск сервера принят к исполнению")
 
-    @discord.ui.button(label="Остановка сервера", style=ButtonStyle.red)
-    async def stop_callback(self, interaction, button):
-        await stop()
-        await interaction.response.send_message("Остановка сервера принята к исполнению")
-
-    @discord.ui.button(label="Остановка сервера через 1 минуту", style=ButtonStyle.red)
-    async def soft_stop_callback(self, interaction, button):
-        await soft_stop()
-        await interaction.response.send_message("Остановка сервера через 1 минуту принята к исполнению")
-
-    @discord.ui.button(label="Перезапуск сервера", style=ButtonStyle.green)
+    @discord.ui.button(label="Перезапуск сервера", style=ButtonStyle.green, row=0)
     async def restart_callback(self, interaction, button):
         await restart()
         await interaction.response.send_message("Перезагрузка сервера принята к исполнению")
 
-    @discord.ui.button(label="Перезапуск сервера через 1 минуту", style=ButtonStyle.green)
+    @discord.ui.button(label="Перезапуск сервера через 1 минуту", style=ButtonStyle.green, row=0)
     async def soft_restart_callback(self, interaction, button):
         await soft_restart()
         await interaction.response.send_message("Перезагрузка сервера через 1 минуту принята к исполнению")
 
-    @discord.ui.button(label="Пересоздание игрового мира", style=ButtonStyle.red)
+    @discord.ui.button(label="Остановка сервера", style=ButtonStyle.red, row=1)
+    async def stop_callback(self, interaction, button):
+        await stop()
+        await interaction.response.send_message("Остановка сервера принята к исполнению")
+
+    @discord.ui.button(label="Остановка сервера через 1 минуту", style=ButtonStyle.red, row=1)
+    async def soft_stop_callback(self, interaction, button):
+        await soft_stop()
+        await interaction.response.send_message("Остановка сервера через 1 минуту принята к исполнению")
+
+    @discord.ui.button(label="Пересоздание игрового мира", style=ButtonStyle.red, row=1)
     async def soft_world_regenerate_callback(self, interaction, button):
         await soft_world_regenerate()
         await interaction.response.send_message("Пересоздание игрового мира принято к исполнению")
