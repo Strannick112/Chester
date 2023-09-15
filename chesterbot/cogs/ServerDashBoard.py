c7d55507feb57844d698c532f1865392a9bb4153
Stanislav <stas960@gmail.com>
2023-09-15 19:54:56 +0300

    Изменение внешнего вида доски подсчета

--- chesterbot/cogs/ServerDashBoard.py
+++ chesterbot/cogs/ServerDashBoard.py
@@ -35,7 +35,7 @@ class ServerDashBoard:
 
     async def make_dashboard(self):
         embed = discord.Embed(colour=discord.Colour.dark_teal())
-        embed.set_thumbnail("https://media.discordapp.net/attachments/871824345780080690/1149833791687368744/2.png")
+        embed.set_thumbnail(url="https://media.discordapp.net/attachments/871824345780080690/1149833791687368744/2.png")
         embed.set_author(name=main_config["server_name"])
         description = main_config["description"]
         description += "\n**День**: " + (await self._get_cycles()).__str__()
