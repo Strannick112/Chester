af64d8918e3635669445f41c2a2e0a914fa5a13d
Stanislav <stas960@gmail.com>
2023-09-15 14:48:54 +0300

    Изменение заголовочного эмбеда для доски подсчета

--- chesterbot/cogs/DashBoardEmbed.py
+++ chesterbot/cogs/DashBoardEmbed.py
@@ -61,8 +61,8 @@ class DashBoardEmbed(commands.Cog, name="Доска подсчёта"):
         head_embed.set_image(url=main_config["main_embed_picture"])
         dashboard = [head_embed]
         dashboard += [await world.make_dashboard() for world in self.world_dashboards]
-        for board in dashboard:
-            board.set_image(url="https://media.discordapp.net/attachments/871824345780080690/1152208878772174848/image.png")
+        for i in range(1, dashboard.__len__()):
+            dashboard[i].set_image(url="https://media.discordapp.net/attachments/871824345780080690/1152208878772174848/image.png")
         try:
             await self.message.edit(embeds=dashboard)
         finally:
