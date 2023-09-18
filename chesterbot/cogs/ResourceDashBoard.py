d440f9eff8576a6fb0c228e52fa4867b90404eb1
Stanislav <stas960@gmail.com>
2023-09-19 00:14:41 +0300

    Изменение внешнего вида доски подсчета

--- chesterbot/cogs/ResourceDashBoard.py
+++ chesterbot/cogs/ResourceDashBoard.py
@@ -12,80 +12,83 @@ class ResourceDashBoard:
         self.screen_name = world["screen_name"]
         self.data = (ResourceDashBoard.__data[world["world_type"]]).copy()
 
-    __data = {
-        "overworld": {
-            "Боссы": {
-                "Дружелюбная фруктовая муха": {"friendlyfruitfly": 0},
-                "Драконья муха": {"dragonfly": 0},
-                "Гломмер": {"glommer": 0},
-                "Мешок клауса": {"klaus_sack": 0},
-                "Улей пчелиной королевы": {"beequeenhivegrown": 0},
-                "Медведь-барсук": {"bearger": 0},
-                "Яйца Гусь-лусей": {"mooseegg": 0},
-                "Гусь-лусь": {"moose": 0},
-                "Лусята": {"mossling": 0},
-                "Муравьиный лев": {"antlion": 0},
-                "Король-краб": {"crabking": 0},
-                "Скелеты игроков": {"skeleton": 0},
+    __overworld = {
+        "Боссы": {
+            "Дружелюбная фруктовая муха": {"friendlyfruitfly": 0},
+            "Драконья муха": {"dragonfly": 0},
+            "Гломмер": {"glommer": 0},
+            "Мешок клауса": {"klaus_sack": 0},
+            "Улей пчелиной королевы": {"beequeenhivegrown": 0},
+            "Медведь-барсук": {"bearger": 0},
+            "Яйца Гусь-лусей": {"mooseegg": 0},
+            "Гусь-лусь": {"moose": 0},
+            "Лусята": {"mossling": 0},
+            "Муравьиный лев": {"antlion": 0},
+            "Король-краб": {"crabking": 0},
+            "Скелеты игроков": {"skeleton": 0},
+    },
+        "Существа":{
+            "Вольт-козы": {"lightninggoat": 0},
+            "Бифало": {"beefalo": 0},
+            "Хижины моржей": {"walrus_camp": 0},
+            "Моржи": {"walrus": 0},
+            "Дома свиней": {"pighouse": 0},
+            "Дома мермов": {"mermhouse": 0},
+            "Мермы": {"merm": 0},
+            "Щупальца": {"tentacle": 0},
+            "Шахматы": {"knight": 0, "bishop": 0, "rook": 0},
+            "Енотокоты": {"catcoon": 0},
+            "Дикие улья": {"beehive": 0},
+            "Улья пчёл-убийц": {"wasphive": 0},
+            "Смертоносные светлоцветы": {"lunarthrall_plant": 0},
         },
-            "Существа":{
-                "Вольт-козы": {"lightninggoat": 0},
-                "Бифало": {"beefalo": 0},
-                "Хижины моржей": {"walrus_camp": 0},
-                "Моржи": {"walrus": 0},
-                "Дома свиней": {"pighouse": 0},
-                "Дома мермов": {"mermhouse": 0},
-                "Мермы": {"merm": 0},
-                "Щупальца": {"tentacle": 0},
-                "Шахматы": {"knight": 0, "bishop": 0, "rook": 0},
-                "Енотокоты": {"catcoon": 0},
-                "Дикие улья": {"beehive": 0},
-                "Улья пчёл-убийц": {"wasphive": 0},
-                "Смертоносные светлоцветы": {"lunarthrall_plant": 0},
-            },
-            "Ресурсы": {
-                "Кусты камыша": {"reeds": 0},
-                "Кусты каменных фруктов": {"rock_avocado_bush": 0},
-                "Перекати-поле": {"tumbleweed": 0},
-                "Спаунеры перекати-поле": {"tumbleweedspawner": 0},
-                "Приманкоцветы": {"lureplant": 0},
-                "Разломы": {"lunarrift_portal": 0},
-                "Кристаллы разлома": {"lunarrift_crystal_big": 0},
-            }
-        },
-        "caves": {
-            "Боссы": {
-                "Древний страж": {"minotaur": 0},
-                "Дружелюбная фруктовая муха": {"friendlyfruitfly": 0},
-                "Кошмарный свин-оборотень": {"daywalker": 0},
-                "Скелеты игроков": {"skeleton": 0},
-            },
-            "Существа": {
-                "Проглоты": {"slurper": 0},
-                "Глубинные черви": {"worm": 0},
-                "Лобстеры": {"rocky": 0},
-                "Дома зайцев": {"rabbithouse": 0},
-                "Логова мотыльков": {"dustmothden": 0},
-                "Обезьяньи тотемы": {"monkeybarrel": 0},
-                "Щупальца": {"tentacle": 0},
-                "Повреждённых шахмат на охране руин": {
-                    "knight_nightmare": 0,
-                    "bishop_nightmare": 0,
-                    "rook_nightmare": 0,
-                },
-                "Спилагмиты": {"spiderhole": 0},
-            },
-            "Ресурсы": {
-                "Статуи в руинах": {
-                    "ruins_statue_mage": 0,
-                    "ruins_statue_mage_nogem": 0,
-                    "ruins_statue_head": 0,
-                    "ruins_statue_head_nogem": 0,
-                },
-                "Кусты камыша": {"reeds": 0},
-                "Сундуки в лабиринте": {"pandoraschest": 0},
-            }
+        "Ресурсы": {
+            "Кусты камыша": {"reeds": 0},
+            "Кусты каменных фруктов": {"rock_avocado_bush": 0},
+            "Перекати-поле": {"tumbleweed": 0},
+            "Спаунеры перекати-поле": {"tumbleweedspawner": 0},
+            "Приманкоцветы": {"lureplant": 0},
+            "Разломы": {"lunarrift_portal": 0},
+            "Кристаллы разлома": {"lunarrift_crystal_big": 0},
         }
+    }
+
+    __caves = {
+        "Боссы": {
+            "Древний страж": {"minotaur": 0},
+            "Дружелюбная фруктовая муха": {"friendlyfruitfly": 0},
+            "Кошмарный свин-оборотень": {"daywalker": 0},
+            "Скелеты игроков": {"skeleton": 0},
+        },
+        "Существа": {
+            "Проглоты": {"slurper": 0},
+            "Глубинные черви": {"worm": 0},
+            "Лобстеры": {"rocky": 0},
+            "Дома зайцев": {"rabbithouse": 0},
+            "Логова мотыльков": {"dustmothden": 0},
+            "Обезьяньи тотемы": {"monkeybarrel": 0},
+            "Щупальца": {"tentacle": 0},
+            "Повреждённых шахмат на охране руин": {
+                "knight_nightmare": 0,
+                "bishop_nightmare": 0,
+                "rook_nightmare": 0,
+            },
+            "Спилагмиты": {"spiderhole": 0},
+        },
+        "Ресурсы": {
+            "Статуи в руинах": {
+                "ruins_statue_mage": 0,
+                "ruins_statue_mage_nogem": 0,
+                "ruins_statue_head": 0,
+                "ruins_statue_head_nogem": 0,
+            },
+            "Кусты камыша": {"reeds": 0},
+            "Сундуки в лабиринте": {"pandoraschest": 0},
+        }
+    }
+    __data = {
+        "overworld": __overworld,
+        "caves": __caves
     }
 
     async def make_dashboard(self):
