aab23ab7ea1e54d038c0c162d0e1deb9a55e8e1b
Stanislav <stas960@gmail.com>
2023-11-17 16:43:16 +0300

    Добавление проверки количества прожитых дней у игроков, подавших заявку

--- chesterbot/cogs/WipeManage.py
+++ chesterbot/cogs/WipeManage.py
@@ -386,7 +386,7 @@ class WipeManage(commands.Cog, name="Управление вайпами"):
             await message.add_reaction(self.__replies['claim_accepted_is_ok'])
             is_days_ok = await claim.check_days(self.chester_bot.console_dst_checker, self.__replies['claim_days_count'])
             if not is_days_ok:
-                await message.add_reaction(self.__replies['claim_days_ok'])
+                await message.add_reaction(self.__replies['claim_warning'])
             return claim
         return None
 
