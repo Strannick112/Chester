8cb1513870652ddd63d4a6d988d4df27bbff48f5
Stanislav <stas960@gmail.com>
2023-12-25 19:53:42 +0300

    Добавление предсказаний со стикерами

--- chesterbot/cogs/Halloween.py
+++ chesterbot/cogs/Halloween.py
@@ -23,7 +23,7 @@ class Halloween(commands.Cog, name="Хеллоуин"):
             for sticker in stickers:
                 await ctx.reply(
                     sticker.id,
-                    stickers=self.chester_bot.get_sticker(sticker.id)
+                    stickers=self.chester_bot.get_sticker((sticker,))
                 )
 
 
