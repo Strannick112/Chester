591eca0bd43d97ef0a76ca0846ea1c0cf4c992f6
Stanislav <stas960@gmail.com>
2023-12-25 19:07:22 +0300

    Добавление предсказаний со стикерами

--- chesterbot/cogs/Halloween.py
+++ chesterbot/cogs/Halloween.py
@@ -21,7 +21,7 @@ class Halloween(commands.Cog, name="Хеллоуин"):
             random_prediction_index = random.randint(0, len(self.chester_bot.replies["predictions"]))
             stickers_id = self.chester_bot.replies["predictions"][random_prediction_index].get("stickers")
             if stickers_id is not None:
-                stickers = [await self.chester_bot.fetch_stickers(stick_id) for stick_id in stickers_id]
+                stickers = [await self.chester_bot.fetch_sticker(stick_id) for stick_id in stickers_id]
                 await ctx.reply(
                     self.chester_bot.replies["predictions"][random_prediction_index].get("text"),
                     stickers=stickers,
