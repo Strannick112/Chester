662d78ccfb26b1c0f19218d4db6a9a0da1bb039e
Stanislav <stas960@gmail.com>
2023-12-25 21:47:52 +0300

    Изменение весов предсказаний

--- chesterbot/cogs/Halloween.py
+++ chesterbot/cogs/Halloween.py
@@ -12,28 +12,27 @@ class Halloween(commands.Cog, name="Хеллоуин"):
     def __init__(self, chester_bot):
         self.chester_bot = chester_bot
         self.__replies = chester_bot.replies
+        self.__weights = tuple(
+            weight if (weight := prediction["weight"]) is not None
+            else 1
+            for prediction in self.chester_bot.replies["predictions"]
+        )
+
     @commands.command(name="magic")
     async def change_replies(self, ctx):
         """
         Узнать предсказание на сегодня.
         """
         if ctx.message.channel.id == self.chester_bot.replies["predictions_channel_id"]:
-            # stickers = await ctx.message.guild.fetch_stickers()
-            # for sticker in stickers:
-            #     await ctx.reply(
-            #         sticker.id,
-            #         stickers=(await self.chester_bot.fetch_sticker(sticker.id),)
-            #     )
-
-            random_prediction_index = random.randint(0, len(self.chester_bot.replies["predictions"]) - 1)
-            stickers_id = self.chester_bot.replies["predictions"][random_prediction_index].get("stickers")
+            prediction = random.choices(self.chester_bot.replies["predictions"], weights=self.__weights, k=1)
+            stickers_id = prediction.get("stickers")
             if stickers_id is not None:
                 stickers = [await self.chester_bot.fetch_sticker(stick_id) for stick_id in (*stickers_id,)]
                 await ctx.reply(
-                    self.chester_bot.replies["predictions"][random_prediction_index].get("text"),
+                    prediction.get("text"),
                     stickers=stickers,
                 )
             else:
                 await ctx.reply(
-                    self.chester_bot.replies["predictions"][random_prediction_index].get("text")
+                    prediction.get("text")
                 )
