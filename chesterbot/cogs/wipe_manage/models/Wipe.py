8d2626e465309d56e7f95a36bf53f16ea23f73c2
Stanislav <stas960@gmail.com>
2024-04-28 21:41:01 +0300

    Изменение отображения информации о вайпе и заявке в дискорде

--- chesterbot/cogs/wipe_manage/models/Wipe.py
+++ chesterbot/cogs/wipe_manage/models/Wipe.py
@@ -35,7 +35,7 @@ class Wipe(Base):
         claims = "[\n"
         status_max_lenght = 12
         for index, claim in enumerate(await self.awaitable_attrs.claims):
-            # claims += "[Заявка](" + str(claim.message_link) + ")ᅠᅠ"
+            claims += "[Заявка](" + str(claim.message_link) + ")ᅠᅠ"
             # status = str(await (await claim.awaitable_attrs.status).name)
             # claims += status + str(((status_max_lenght - len(status)) // 2) * "ᅠ")
             claims += f"ᅠᅠ{index + 1}. <@" + str(
