6d68a201d784b7173ff2f642f703fe1906449cbc
Stanislav <stas960@gmail.com>
2024-10-20 14:40:48 +0300

    Теперь is_event в config.py работает

--- chesterbot/cogs/Halloween.py
+++ chesterbot/cogs/Halloween.py
@@ -2,6 +2,8 @@ import random
 
 from discord.ext import commands
 
+from chesterbot import main_config
+
 
 class Halloween(commands.Cog, name="Хеллоуин"):
     def __init__(self, chester_bot):
@@ -18,16 +20,17 @@ class Halloween(commands.Cog, name="Хеллоуин"):
         """
         Узнать предсказание на сегодня.
         """
-        if ctx.message.channel.id == self.chester_bot.replies["predictions_channel_id"]:
-            prediction = random.choices(self.chester_bot.replies["predictions"], weights=self.__weights, k=1)[0]
-            stickers_id = prediction.get("stickers")
-            if stickers_id is not None:
-                stickers = [await self.chester_bot.fetch_sticker(stick_id) for stick_id in (*stickers_id,)]
-                await ctx.reply(
-                    prediction.get("text"),
-                    stickers=stickers,
-                )
-            else:
-                await ctx.reply(
-                    prediction.get("text")
-                )
+        if main_config["is_event"]:
+            if ctx.message.channel.id == self.chester_bot.replies["predictions_channel_id"]:
+                prediction = random.choices(self.chester_bot.replies["predictions"], weights=self.__weights, k=1)[0]
+                stickers_id = prediction.get("stickers")
+                if stickers_id is not None:
+                    stickers = [await self.chester_bot.fetch_sticker(stick_id) for stick_id in (*stickers_id,)]
+                    await ctx.reply(
+                        prediction.get("text"),
+                        stickers=stickers,
+                    )
+                else:
+                    await ctx.reply(
+                        prediction.get("text")
+                    )
