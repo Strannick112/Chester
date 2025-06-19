0b4ed7e9a6fde014a20ece9e69a259789c6b0f3a
Stanislav <stas960@gmail.com>
2025-06-19 22:41:26 +0300

    Рефакторинг

--- chesterbot/cogs/ScreenEmbed.py
+++ chesterbot/cogs/ScreenEmbed.py
@@ -6,19 +6,19 @@ from chesterbot.cogs.SavedMessage import SavedMessage
 class ScreenEmbed:
     def __init__(self, name, channel, bot, head_picture, embed_list_default, view, update_callback):
         self.chester_bot = bot
-        self.saved_embed_message = SavedMessage(
-            name, channel, self.chester_bot, {"embeds": embed_list_default, "view": view}
-        )
         self.saved_picture_message = SavedMessage(
             name + "_picture", channel, self.chester_bot, {"file": head_picture}
+        )
+        self.saved_embed_message = SavedMessage(
+            name, channel, self.chester_bot, {"embeds": embed_list_default, "view": view}
         )
         self.embed_list = None
         self.view = view
         self.update_callback = update_callback
 
     async def on_ready(self):
-        await self.saved_embed_message.on_ready()
         await self.saved_picture_message.on_ready()
+        await self.saved_embed_message.on_ready()
         self.reload_data.start()
 
     async def update_dashboard(self):
