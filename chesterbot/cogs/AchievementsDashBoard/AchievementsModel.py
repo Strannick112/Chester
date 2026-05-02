7d74039ce93a1128483341afe431760954598d37
User <stas960@gmail.com>
2026-05-02 12:22:36 +0300

    Добавление считывания папки с сохранениями мира Master

--- chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
@@ -1,9 +1,11 @@
 from chesterbot import main_config
+from chesterbot.cogs.AchievementsDashBoard.AchievementsReader import AchievementsReader
 
 
 class AchievementsModel:
     def __init__(self):
         self._data = []
+        self.reader = AchievementsReader()
 
     async def update_data(self):
         self._data = [
@@ -21,5 +23,6 @@ class AchievementsModel:
 
     async def get_data(self):
         await self.update_data()
+        self.reader.get_session_folder()
         return self._data
 
