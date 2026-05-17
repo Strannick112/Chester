c57763df4bc7bcd8630e69e7981bccfd13323821
Stanislav <stas960@gmail.com>
2026-05-17 21:15:46 +0300

    Добавление заголовочного изображения

--- chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
@@ -9,7 +9,8 @@ class AchievementsModel:
 
     async def update_data(self):
         self._data = []
-        for nick, points in self.reader.player_points:
+        for player_info in self.reader.player_points:
+            nick, points = next(iter(player_info.items()))
             print(f"""meaw: { { "Никнейм": nick, "Очки": str(points) } }""")
             self._data.append({ "Никнейм": nick, "Очки": str(points) })
         print(f"self._data: {self._data}")
