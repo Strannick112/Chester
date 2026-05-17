717c2607407b16786f0ae7bb3470950436f92e24
Stanislav <stas960@gmail.com>
2026-05-17 19:54:17 +0300

    Добавление подсчета очков за достижения

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -56,6 +56,8 @@ class AchievementsReader():
                 start = text.find('{')
                 end = text.rfind('}')
                 clean_json = text[start:end + 1]
+                end = clean_json.rfind('}')
+                clean_json = clean_json[start:end + 1]
 
                 data = data = luadata.unserialize(clean_json)["data"]["kaachievementmanager"]
                 #data = json.loads(json_content.decode('utf-8'))["data"]["kaachievementmanager"]
