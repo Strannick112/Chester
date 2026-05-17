001a369445a879ad50f527f104d19505560a143a
Stanislav <stas960@gmail.com>
2026-05-17 20:02:30 +0300

    Изменение отображения таблицы рейтинга

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -57,7 +57,7 @@ class AchievementsReader():
                 end = text.rfind('}')
                 clean_json = text[start:end]
                 end = clean_json.rfind('}')
-                clean_json = clean_json[:end]
+                clean_json = clean_json[:end + 1]
                 print("clean_json:", clean_json)
                 data = data = luadata.unserialize(clean_json)["data"]["kaachievementmanager"]
                 #data = json.loads(json_content.decode('utf-8'))["data"]["kaachievementmanager"]
