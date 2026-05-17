1d57ec3451a31968fd198116ced3ec242059f875
Stanislav <stas960@gmail.com>
2026-05-17 19:33:37 +0300

    Добавление подсчета очков за достижения

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -50,7 +50,9 @@ class AchievementsReader():
         data = None
         for file_name in self.player_saves:
             with open(file_name, 'r', encoding='utf-8') as file:
-                data = json.load(file)["data"]["kaachievementmanager"]
+                json_content = file.read()[10:]
+                data = json.loads(json_content.decode('utf-8'))["data"]["kaachievementmanager"]
+                # data = json.load(file)["data"]["kaachievementmanager"]
             print("data: ", data)
             cur_points = 0
             for field_name, field_value in data:
