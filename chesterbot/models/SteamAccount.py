1eabf19ef86e90bbbfc8d9b90d8fe3d7288b3083
Stanislav <stas960@gmail.com>
2026-07-14 14:21:11 +0300

    Прикрепление статистики игрока к его никнейму

--- chesterbot/models/SteamAccount.py
+++ chesterbot/models/SteamAccount.py
@@ -34,4 +34,4 @@ class SteamAccount(Base):
 
     @staticmethod
     async def get_by_ku_id(session, ku_id):
-        return await session.execute(select(SteamAccount).filter_by(ku_id=ku_id)).scalars().first()
+        return (await session.execute(select(SteamAccount).filter_by(ku_id=ku_id))).scalars().first()
