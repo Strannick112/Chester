950327a7195a84005346118cfcc6c0af02c32e8e
Stanislav <stas960@gmail.com>
2026-07-14 17:33:25 +0300

    Корректировка ширины столбцов

--- chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
@@ -10,9 +10,9 @@ class AchievementsModel:
         self._data = []
         for player_info in self.reader.player_points:
             nick, points = next(iter(player_info.items()))
-            print(f"""meaw: { { "Никнейм": nick, "Очки": str(points) } }""")
+            # print(f"""meaw: { { "Никнейм": nick, "Очки": str(points) } }""")
             self._data.append({ "Никнейм": nick, "Очки": str(points) })
-        print(f"self._data: {self._data}")
+        # print(f"self._data: {self._data}")
 
     async def get_data(self):
         await self.reader.update_player_points()
--- chesterbot/models/SteamAccount.py
+++ chesterbot/models/SteamAccount.py
@@ -34,5 +34,4 @@ class SteamAccount(Base):
 
     @staticmethod
     async def get_by_ku_id(session, ku_id):
-        print("meaw inside")
         return (await session.execute(select(SteamAccount).filter_by(ku_id=ku_id))).scalars().first()
