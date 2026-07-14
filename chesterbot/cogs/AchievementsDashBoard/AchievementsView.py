a93c3977a91cc41792fe4dc64c9ddec2b1af2555
Stanislav <stas960@gmail.com>
2026-07-14 17:21:33 +0300

    Прикрепление статистики игрока к его никнейму

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -12,7 +12,7 @@ class AchievementsView(discord.ui.View):
         embed = discord.Embed(color=0x2F3136, title="РЕЙТИНГОВАЯ ТАБЛИЦА")
         # header = "Сезон: 1\nОбновлен: 26.04.2024\nДо конца сезона: 26\n"
 
-        embed.add_field(name="", value="Сезон: 1\n", inline=True)
+        embed.add_field(name="", value="Сезон: 1                       \n", inline=True)
         # embed.add_field(name="", value="Обновлен: 26.04.2024\n", inline=True)
         # embed.add_field(name="", value="До конца сезона: 26\n", inline=True)
         embed.add_field(name="", value="\n", inline=False)
