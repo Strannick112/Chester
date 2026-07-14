5a9680298fbf46b207a6b239f05738c3843856b5
Stanislav <stas960@gmail.com>
2026-07-14 14:33:28 +0300

    Прикрепление статистики игрока к его никнейму

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -69,10 +69,11 @@ class AchievementsReader():
                     else:
                         cur_points += field_value * points
             player_name = None
-
+            print(f"cur_points: {cur_points}")
 
             async with self.chester_bot.async_session() as session:
                 async with session.begin():
                     player_name = ( await SteamAccount.get_by_ku_id(session=session, ku_id=ku_id) ).nickname
+            print(f"player_name: {player_name}")
             if player_name is not None:
                 self.player_points.append( { player_name: cur_points } )
