88ae9598b71e3aedb9d309eb144248ab45f7bf9a
Stanislav <stas960@gmail.com>
2026-07-24 15:36:01 +0300

    Проверка расчета количества очков

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -8,7 +8,7 @@ class AchievementsView(discord.ui.View):
     def __init__(self, model):
         super().__init__()
         self.model = model
-        self.column_size = 12
+        self.column_size = 16
 
     def column_size_corrector(self, column_headers):
         return [
