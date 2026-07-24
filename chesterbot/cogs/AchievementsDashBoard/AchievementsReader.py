386058696a731a8ed7cf4eaa2a399c09197de3c5
Stanislav <stas960@gmail.com>
2026-07-24 13:53:35 +0300

    Проверка расчета количества очков

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -79,13 +79,12 @@ class AchievementsReader():
             cur_points = 0
             for field_name, field_value in data.items():
                 if (points := achievements_list.get(field_name)) is not None:
-                    if isinstance(field_value, dict):
+                    # if isinstance(field_value, dict):
                         cur_points += points
-
-                    else:
-                        cur_points += field_value * points
+                    # else:
+                    #     cur_points += field_value * points
                     print(f"{field_name}: points: {points}, cur_points: {cur_points}")
 
             self.player_points.append( { player_name: cur_points } )
-        # self.player_points = [player for player in self.player_points if next(iter(player.values())) > 200]
+        self.player_points = [player for player in self.player_points if next(iter(player.values())) > 200]
         self.player_points = sorted(self.player_points, key=lambda player: next(iter(player.values())), reverse=True)
