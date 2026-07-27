33fdd32b95c62302e6e41021886628d74eba6ca3
Stanislav <stas960@gmail.com>
2026-07-27 19:56:44 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsModel.py
@@ -8,18 +8,13 @@ class AchievementsModel:
 
     async def update_data(self):
         self._data = []
-        print("meaw14")
         for player_info in self.reader.player_points:
             nick, points = next(iter(player_info.items()))
             # print(f"""meaw: { { "Никнейм": nick, "Очки": str(points) } }""")
             self._data.append({ "Никнейм": nick, "Очки": str(points) })
-        print("meaw15")
         # print(f"self._data: {self._data}")
 
     async def get_data(self):
-        print("meaw10")
         await self.reader.update_player_points()
-        print("meaw11")
         await self.update_data()
-        print("meaw13")
         return self._data
