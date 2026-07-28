ec8f001377ddcc9c19d0fb9611f934de6a019419
Stanislav <stas960@gmail.com>
2026-07-28 18:56:27 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -17,48 +17,47 @@ class AchievementsView:
         ]
 
     async def _make_dashboard(self):
-        # embed = discord.Embed(color=0x2F3136, title="РЕЙТИНГ ИГРОКОВ")
-        embed = discord.Embed(color=0x2F3136, title="")
-        # embed.set_thumbnail(url=main_config["achievements_tumbnail"])
-        # headers = self.column_size_corrector((
-        #     "Сезон: 1",
-        #     "",
-        #     "",
-        # ))
-        headers = self.column_size_corrector((
-            "",
-            "",
-            "",
-        ))
-        for header in headers:
-            embed.add_field(name="", value=header, inline=True)
-        # embed.add_field(name="", value="Сезон: 1\n", inline=True)
-        # embed.add_field(name="", value="Обновлен: 26.04.2024\n", inline=True)
-        # embed.add_field(name="", value="До конца сезона: 26\n", inline=True)
-        embed.add_field(name="", value="\n", inline=False)
         data = await self.model.get_data()
-        player_nickname_column = ""
-        player_points_column = ""
-        rangs_column = ""
+        players_by_rang = dict()
         for player in data:
             points = player.get('Очки')
-            player_nickname_column += player.get("Никнейм")
-            player_nickname_column += "\n"
-            player_points_column += f"⠀⠀⠀Очки: {str(points)}"
-            player_points_column += "\n"
-            cur_rang = ""
             for rang_info in rang_list.items():
                 if int(rang_info[1]) < int(points):
                     cur_rang = rang_info[0]
                 else:
                     break
-            # print(f"cur_rang: {cur_rang}")
-            rangs_column += f"⠀⠀Ранг: {cur_rang}"
-            rangs_column += "\n"
-        embed.add_field(name="", value=player_nickname_column, inline=True)
-        embed.add_field(name="", value=player_points_column, inline=True)
-        embed.add_field(name="", value=rangs_column, inline=True)
-        return embed
+                if players_by_rang.get(cur_rang, None) is None:
+                    players_by_rang[cur_rang] = [player]
+                else:
+                    players_by_rang[cur_rang].append(player)
+        rang_embeds = []
+        for rang, players in players_by_rang.items():
+            rang_embed = discord.Embed(color=0x2F3136, title="")
+            headers = self.column_size_corrector((
+                "",
+                "",
+                "",
+            ))
+            for header in headers:
+                rang_embed.add_field(name="", value=header, inline=True)
+            rang_embed.add_field(name="", value="\n", inline=False)
+            rang_embed.set_thumbnail(url=main_config["achievements_tumbnail"])
+            player_nickname_column = ""
+            player_points_column = ""
+            rangs_column = ""
+            for player in players:
+                points = player.get('Очки')
+                player_nickname_column += player.get("Никнейм")
+                player_nickname_column += "\n"
+                player_points_column += f"⠀⠀⠀Очки: {str(points)}"
+                player_points_column += "\n"
+                rangs_column += f"⠀⠀Ранг: {rang}"
+                rangs_column += "\n"
+            rang_embed.add_field(name="", value=player_nickname_column, inline=True)
+            rang_embed.add_field(name="", value=player_points_column, inline=True)
+            rang_embed.add_field(name="", value=rangs_column, inline=True)
+            rang_embeds.append(rang_embed)
+        return rang_embeds
 
     async def update(self):
         embed = discord.Embed(color=0x2F3136, title="РЕЙТИНГ ИГРОКОВ")
@@ -71,7 +70,7 @@ class AchievementsView:
         embed.set_image(url=main_config["achievement_embed_law_picture"])
         for header in headers:
             embed.add_field(name="", value=header, inline=True)
-        embeds = [embed, await self._make_dashboard()]
+        embeds = [embed, *(await self._make_dashboard())]
 
         # view = discord.ui.View()
         # style = discord.ButtonStyle.gray
