0591b6b7bf7c417b55aac3d5d1a75281fd4d9381
Stanislav <stas960@gmail.com>
2026-07-29 18:55:14 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -65,8 +65,8 @@ class AchievementsView:
         embed.set_thumbnail(url=main_config["achievement_embed_law_picture"])
         headers = (
             "Сезон: 1⠀⠀⠀⠀⠀⠀",
-            "Порог входа: 200 очков⠀⠀",
-            "Завершение: 18.09⠀⠀⠀⠀⠀",
+            "Порог входа: 200 очков⠀",
+            "Завершение: 18.09⠀⠀⠀⠀",
         )
         # embed.set_image(url=main_config["achievement_embed_law_picture"])
         for header in headers:
