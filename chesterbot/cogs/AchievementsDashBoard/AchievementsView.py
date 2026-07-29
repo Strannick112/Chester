96f40c989712f9149bb32fb476a428c9a8bf5d63
Stanislav <stas960@gmail.com>
2026-07-29 18:53:41 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -62,13 +62,13 @@ class AchievementsView:
 
     async def update(self):
         embed = discord.Embed(color=0x2F3136, title="РЕЙТИНГ ИГРОКОВ")
-        # embed.set_thumbnail(url=main_config["achievements_tumbnail"])
+        embed.set_thumbnail(url=main_config["achievement_embed_law_picture"])
         headers = (
             "Сезон: 1⠀⠀⠀⠀⠀⠀",
             "Порог входа: 200 очков⠀⠀⠀⠀",
             "Завершение: 18.09⠀⠀⠀⠀⠀⠀⠀",
         )
-        embed.set_image(url=main_config["achievement_embed_law_picture"])
+        # embed.set_image(url=main_config["achievement_embed_law_picture"])
         for header in headers:
             embed.add_field(name="", value=header, inline=True)
         embeds = [embed, *(await self._make_dashboard())]
