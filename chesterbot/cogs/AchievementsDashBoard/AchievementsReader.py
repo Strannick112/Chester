5df25d12d8f2972f6965dfe672d2fa450d1f4672
Stanislav <stas960@gmail.com>
2026-07-31 23:08:36 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -89,7 +89,9 @@ class AchievementsReader():
                     else:
                         cur_points += min(field_value, 1) * points
                     if field_name == "numSurvivedDay":
+                        print(f"before: player_name: {player_name}, {field_name}: points: {points}, cur_points: {cur_points}")
                         cur_points += field_value * points
+                        print(f"before: {field_name}: points: {points}, cur_points: {cur_points}")
                     # print(f"{field_name}: points: {points}, cur_points: {cur_points}")
             self.player_points.append( { player_name: cur_points } )
         self.player_points = [player for player in self.player_points if next(iter(player.values())) > 200]
