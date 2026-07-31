d7e5045e0b26fe35260f676535e5f2f0cc5f4b58
Stanislav <stas960@gmail.com>
2026-07-31 23:51:50 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsReader.py
@@ -49,6 +49,7 @@ class AchievementsReader():
         self.player_points = []
         data = None
         for ku_id, file_name in self._get_player_saves():
+            print(f"player_info: ku_id: {ku_id}, file_name: {file_name}")
             if file_name is None:
                 continue
             player_name = None
@@ -59,7 +60,7 @@ class AchievementsReader():
                         player_name = player.nickname
             if player_name is None:
                 continue
-            print(f"player_info: player_name: {player_name}, ku_id: {ku_id}, file_name: {file_name}")
+            print(f"player_info: player_name: {player_name}")
             with open(file_name, 'rb') as file:
                 content = file.read()
                 text = content.decode('utf-8', errors='ignore')
