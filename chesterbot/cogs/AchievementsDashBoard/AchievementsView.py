8f2c77284b9aca1f291b0f0e1097b9f6db3f498e
Stanislav <stas960@gmail.com>
2026-08-01 03:59:54 +0300

    Редизайн

--- chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
+++ chesterbot/cogs/AchievementsDashBoard/AchievementsView.py
@@ -82,6 +82,6 @@ class AchievementsView:
         #             url=button_description["url"]
         #         )
         #     )
-
+        print("Поля первого эмбеда:", embeds[2].fields)
         # return { "embeds": embeds, "view": view }
         return { "embeds": embeds }
